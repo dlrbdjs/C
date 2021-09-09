c6f23df684f67272b84b4550ecca363c82c1c213
unknown <202110958@sangmyung.kr>
2021-09-09 21:07:20 +0900

    hello.c

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+int main(void) {
+	printf("Hello world");
+	return 0;
+}
+*/
+
+/*
+int main(void) {
+	printf("홍길동 \n홍 길 동 \n홍  길  동\n");
+}
+*/
+
+/*
+int main(void) {
+	printf("이름 : 이규언\n");
+	printf("주소 : 경기도\n");
+	printf("전화번호 : 010-1111-2222\n");
+}
+*/
+
+int main(void) {
+	printf("C프로그래밍 1주차 과제\n");
+	printf("이름 : (이규언)\n");
+	printf("학번 : (%d)\n", 202110958);
+}
