236722fefa0ce16945d90da9ad61f05d683752fe
unknown <202110958@sangmyung.kr>
2021-09-14 20:21:57 +0900

    hw1.c

---
+++ hw1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int num1, num2 = 0;
+
+	printf("정수 두개를 입력하세요 : ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d & %d = %d\n", num1, num2, num1 & num2);
+	printf("%d | %d = %d\n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d\n", num1, num2, num1 ^ num2);
+}
